94c54f104c20f0332adcbbc0d24678c3aa38d9d1
johnjspiteri <john@johnspiteri.io>
2018-07-05 23:47:45 -0400

    Updated Material Model & Question-Map

--- model/model.material.js
+++ model/model.material.js
@@ -3,10 +3,9 @@
 var mongoose = require('mongoose'),
 	MaterialSchema = new mongoose.Schema({
 		author: String,
-		date: Date,
 		title: String,
 		type: String,
 		url: String,
-	}, {_id: false});
+	}, { _id: false });
 
 module.exports = mongoose.model('Material', MaterialSchema);
